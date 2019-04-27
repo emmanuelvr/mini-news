1ce7d3e537ac4c4bb1a54664528fe37432f6bb3c
Emmanuel Villalobos <emmanuel.villalobos@gmail.com>
2019-04-27 17:17:37 -0500

    Add Navigation interface

---
+++ src/interfaces/navigation.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface Navigation {
+  navigate: (screen: string, params?: any[]) => void;
+  getParam: (param: string) => any;
+  toggleDrawer: () => void;
+  goBack: () => void;
+}
+
+export default Navigation;
