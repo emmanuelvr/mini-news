d4b89ff23485859409cf0d4390d72f11dbba4601
Emmanuel Villalobos <emmanuel.villalobos@gmail.com>
2019-04-27 16:39:58 -0500

    Change return type to JSX.Element in render of PostsScreen

--- src/screens/posts/index.tsx
+++ src/screens/posts/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import { Text } from 'native-base';
 
-function PostsScreen(): React.ReactNode {
+function PostsScreen(): JSX.Element {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Posts screen</Text>
