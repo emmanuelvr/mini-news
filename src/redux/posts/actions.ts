af16cc09789fafcc4b5c5af665b86fcbb2433332
Emmanuel Villalobos <emmanuel.villalobos@gmail.com>
2019-04-27 19:48:00 -0500

    Add fetchPosts and filterReadPosts actions to posts actions

--- src/redux/posts/actions.ts
+++ src/redux/posts/actions.ts
@@ -1,6 +1,12 @@
-import { POSTS_SET } from '../constants';
+import axios from 'axios';
 
-function setPosts(data: {}): Function {
+import { POSTS_SET } from '../constants';
+// interfaces
+import ReduxModel from '../../interfaces/redux-model';
+import Post from '../../interfaces/post';
+import { PostsState } from './reducer';
+
+function setPosts(data: {}): any {
   return (dispatch: Function): void => {
     dispatch({
       type: POSTS_SET,
@@ -9,6 +15,27 @@ function setPosts(data: {}): Function {
   };
 }
 
+function filterReadPosts(posts: Post[], postsState: PostsState): Post[] {
+  const { readPosts } = postsState;
+  const newPosts = posts.map(
+    (post: Post, index: number): Post => ({
+      ...post,
+      read: readPosts.includes(post.id) || index <= 20,
+    }),
+  );
+  return newPosts;
+}
+
+function fetchPosts(): any {
+  return async (dispatch: Function, getState: Function): void => {
+    const state: ReduxModel = getState();
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const posts = filterReadPosts(data, state.posts);
+    dispatch(setPosts(posts));
+  };
+}
+
 export default {
   setPosts,
+  fetchPosts,
 };
