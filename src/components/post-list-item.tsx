1ea3d96bda12c05aba3254392077dbfe68cba640
Emmanuel Villalobos <emmanuel.villalobos@gmail.com>
2019-05-01 14:14:29 -0500

    Add post is favorite indicator in PostListItem

--- src/components/post-list-item.tsx
+++ src/components/post-list-item.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { ListItem, Text } from 'native-base';
+import { ListItem, Text, Icon } from 'native-base';
 // interfaces
 import Post from '../interfaces/post';
 import UnreadIndicator from './unread-indicator';
 import { withNavigation } from 'react-navigation';
 import Navigation from '../interfaces/navigation';
+// styles
+import commonColor from '../../theme/variables/commonColor';
 
 interface Props {
   post: Post;
@@ -23,6 +25,12 @@ const PostListItem = (props: Props): JSX.Element => {
       style={{ paddingHorizontal: 10 }}
     >
       {!post.read && <UnreadIndicator />}
+      {post.isFavorite && (
+        <Icon
+          name="star"
+          style={{ color: commonColor.brandWarning, fontSize: 24, marginRight: 5 }}
+        />
+      )}
       <Text>{post.title}</Text>
     </ListItem>
   );
